34b17d4c989d72a4c2ed27d9df47f4a202a2247d
Stas Fedorko <stasfedorko@MacBook-Pro-Stas.local>
2020-03-26 18:10:44 +0200

    Updates

---
+++ precache-manifest.312a22c059da70343d171e6c475ca6c6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1aab0e088dcd9fc9ec03673ccabd2525",
+    "url": "/index.html"
+  },
+  {
+    "revision": "97975fa47aa69766fcad",
+    "url": "/static/css/main.4124717e.chunk.css"
+  },
+  {
+    "revision": "652a599d1a99efccd345",
+    "url": "/static/js/2.a430f49c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a430f49c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97975fa47aa69766fcad",
+    "url": "/static/js/main.54230633.chunk.js"
+  },
+  {
+    "revision": "415e9bcf6c081ea50565",
+    "url": "/static/js/runtime-main.f8c5b4be.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
